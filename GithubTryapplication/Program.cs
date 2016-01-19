76fb5930b648256734317e5970a17b9984b58184
Randray <jhfanta@gmail.com>
2016-01-19 17:16:56 +0200

    Intial commit.

---
+++ GithubTryapplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTryapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Gtihub! :3");
+        }
+    }
+}
